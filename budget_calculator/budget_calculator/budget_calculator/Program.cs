7d4f30b06765446f0c5627410d691e7992e1934d
Geoff Brown <geoffb1337@gmail.com>
2021-04-29 12:29:32 -0400

    first budget calculator commit

---
+++ budget_calculator/budget_calculator/budget_calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace budget_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Budget Calculator");
+
+            static void TopMenu()
+            {
+                Console.Write("Please make a selection: ");
+                int selection = Convert.ToInt32(Console.ReadLine());
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine(" 1: View Current Budget");
+                        break;
+                    case 2:
+                        Console.WriteLine(" 2: Update Budget");
+                        break;
+                    default:
+                        Console.WriteLine("Default case");
+                        break;
+                }
+            }
+        }
+    }
+}
